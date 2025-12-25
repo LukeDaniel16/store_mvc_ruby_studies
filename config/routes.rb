96652f63ff7a56df0a8a2aa8657203c004185ccb
Luke Daniel <lucasfelepe@gmail.com>
2025-12-25 17:28:36 -0300

    feat: add controllers and routes of products

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do  
   get "up" => "rails/health#show", as: :rails_health_check  
 
-  get "/products", to: "products#index"
-
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-
-  get "/products/:id", to: "products#show"
-
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
-
-  delete "/products/:id", to: "products#destroy"
+  root "products#index"
+  resources :products
 end
 
 #               CRUD Routers
